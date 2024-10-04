e9022b15697c774566840e8062b20f1983f74ff0
itdenis <denis.maxa4@mail.ru>
2024-10-05 00:45:32 +1000

    lessons 10

--- create_db.py
+++ create_db.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host='127.0.0.1', user='root', passwd='*')
+mydb = mysql.connector.connect(host='127.0.0.1', user='root', passwd='')
 
 my_cursor = mydb.cursor()
 
--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # Old SQLite DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # New MySql DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:*@127.0.0.1/our_users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@127.0.0.1/our_users'
 # Secret Key!
 app.config['SECRET_KEY'] = 'my super secret key that no one is supposed to know'
 # Initialize The Database
@@ -40,6 +40,26 @@ class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+# Update Database Record
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == 'POST':
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        try:
+            db.session.commit()
+            flash('User Updated Successfully!')
+            return render_template('update.html', form=form, name_to_update=name_to_update)
+        except:
+            flash('Error! Looks like there was a problem...try again!')
+            return render_template('update.html', form=form, name_to_update=name_to_update)
+    else:
+        return render_template('update.html', form=form, name_to_update=name_to_update)
+
 
 
 # Create a Form Class
