ee5420646822a6df30597196fc753917b0d1884f
itdenis <denis.maxa4@mail.ru>
2024-10-05 02:37:40 +1000

    lessons 11

--- hello.py
+++ hello.py
@@ -2,47 +2,50 @@ from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-# Create a Flask Instance
+# Создать экземпляр Flask
 app = Flask(__name__)
 
-# Add Database
-# Old SQLite DB
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# New MySql DB
+# Добавить базу данных
+# Новая БД MySql
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@127.0.0.1/our_users'
-# Secret Key!
-app.config['SECRET_KEY'] = 'my super secret key that no one is supposed to know'
-# Initialize The Database
+# Секретный ключ!
+app.config['SECRET_KEY'] = 'мой суперсекретный ключ, который никто не должен знать'
+
+# Инициализировать Базу Данных
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-# Create Model
+# Создать модель
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
+    favorite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
-    # Create a String
+    # Создать строку
     def __repr__(self):
-        return '<Name %r>' % self.name
+        return '<User %r>' % self.name
 
 
 with app.app_context():
     db.create_all()
 
 
-# Create a Form Class
+# Создать класс формы
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
+    favorite_color = StringField('Favorite Color')
     submit = SubmitField('Submit')
 
 
-# Update Database Record
+# Обновить запись базы данных
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     form = UserForm()
@@ -50,38 +53,25 @@ def update(id):
     if request.method == 'POST':
         name_to_update.name = request.form['name']
         name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['favorite_color']
         try:
             db.session.commit()
-            flash('User Updated Successfully!')
+            flash('User successfully updated!')
             return render_template('update.html', form=form, name_to_update=name_to_update)
         except:
-            flash('Error! Looks like there was a problem...try again!')
+            flash('Error! There seems to be an issue... try again!')
             return render_template('update.html', form=form, name_to_update=name_to_update)
     else:
         return render_template('update.html', form=form, name_to_update=name_to_update)
 
 
-
-# Create a Form Class
+# Создать класс формы
 class NamerForm(FlaskForm):
-    name = StringField("What's your name", validators=[DataRequired()])
+    name = StringField("What is your name?", validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
-# Create a route decorator
-
-'''
-FILTERS!!
-safe
-capitalize
-lower
-upper
-title
-trim
-striptags
-'''
-
-
+# Создать декоратор маршрутов
 @app.route('/user/add', methods=['GET', 'POST'])
 def add_user():
     name = None
@@ -89,13 +79,14 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
+            user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
             db.session.add(user)
             db.session.commit()
-        name = form.name.data
-        form.name.data = ''
-        form.email.data = ''
-        flash('User Added Successfully!')
+            name = form.name.data
+            form.name.data = ''
+            form.email.data = ''
+            form.favorite_color.data = ''
+            flash('User successfully added!')
     our_users = Users.query.order_by(Users.date_added)
     return render_template('add_user.html', form=form, name=name, our_users=our_users)
 
@@ -103,41 +94,40 @@ def add_user():
 @app.route('/')
 def index():
     first_name = 'John'
-    stuff = 'This is bold Text'
+    stuff = 'This is bold text'
     favorite_pizza = ['Pepperoni', 'Cheese', 'Mushrooms', 41]
     return render_template('index.html', first_name=first_name, stuff=stuff, favorite_pizza=favorite_pizza)
 
 
-# localhost:5000/user/John
+# локальный хост:5000/user/John
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', user_name=name)
 
 
-# Create Custom Error Pages
-
-# Invalid URL
+# Создание Пользовательских Страниц Ошибок
+# Неверный URL
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 
-# Internal Server Error
+# Внутренняя ошибка сервера
 @app.errorhandler(500)
-def page_not_found(e):
+def internal_error(e):
     return render_template('500.html'), 500
 
 
-# Create Name Page
+# Создать страницу с именем
 @app.route('/name', methods=['GET', 'POST'])
 def name():
     name = None
     form = NamerForm()
-    # Validate Form
+    # Подтвердить Форму
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
-        flash('For Submitted Successfully!')
+        flash('Form submitted successfully!')
     return render_template('name.html', name=name, form=form)
 
 
